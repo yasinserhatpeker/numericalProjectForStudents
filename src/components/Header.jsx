371efd01410e40edda51b2d0b83396a92d38db71
Yasin <yasinpeker43@gmail.com>
2025-05-03 10:30:37 +0300

    added tutorial-app logo to the Header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import {logo} from '../assets/logo.png';
 export default function Header() {
   return (
     <>
-    
+     <div id="header">
+       <img src={logo} alt="a tutorial-app logo" />
+       <h1>
+        
+       </h1>
+     </div>
     </>
   )
 }
