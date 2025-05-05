74ac19b3e6fed0dcb6fea96bf0b8d9969928593d
Yasin <yasinpeker43@gmail.com>
2025-05-05 12:30:49 +0300

    navbar css is done.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
+
+import reservedLogo from '../assets/reserved.webp'
 export default function Footer() {
     return (
         <footer id="footer">
           
-             <img src="" alt="" />
+             <img src={reservedLogo} alt="" />
              <p>All rights are reserved.</p>
-            <img src="" alt="" />
+            <img src={reservedLogo} alt="" />
            </footer>
     )
 }
