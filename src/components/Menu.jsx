b7b7e58f16669cddd522eac35164dbf2d748bc77
Yasin <yasinpeker43@gmail.com>
2025-05-05 10:02:08 +0300

    added one method element.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,19 @@
 import mathLogo from '../assets/math-logo.jpg';
+import bisectionLogo from '../assets/19.03.02-Bisection-method.png';
 export default function Menu() {
 
     return (
         <div id='menu'>
-         
-          <img src={mathLogo} alt="" />
+         <img src={mathLogo} alt="" />
           <h3>Methods Section</h3>
+          <div className="sections">
+             
+              <img src={bisectionLogo} alt="" />
+             <h4>Bisection Method</h4>
+             <button>Click for bisection</button>
+
+            
+          </div>
         </div>
     )
 }
